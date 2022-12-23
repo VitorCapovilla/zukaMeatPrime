113670c281688a4f862cbf615ae0d6b37691723a
Vitor Augusto Capovilla <suporte@mastsolucoes.com>
2022-12-23 01:18:33 -0300

    Tabelas do banco de dados refatorados

--- db/SQL.sql
+++ db/SQL.sql
@@ -1,31 +1,34 @@
 CREATE DATABASE bd_meatprime;
 
-CREATE TABLE categoria(
+CREATE TABLE CATEGORIAS(
     codigo int(6) primary key AUTO_INCREMENT,
     titulo varchar(50) not null,
-    autor varchar(50),
-    datahora varchar(20));
+    autor int(6),
+    datahora date
+);
 
-CREATE TABLE produtos(
+CREATE TABLE PRODUTOS(
     codigo int(6) primary key AUTO_INCREMENT,
     titulo varchar(100) not null,
-    categoria varchar(50),
+    categoria int(6),
     imagem varchar(50),
     descricao varchar(50),
     peso varchar(50),
     preco varchar(50),
-    autor varchar(100),
-    datahora varchar(20));
+    autor int(6),
+    datahora date
+);
 
-CREATE TABLE videos(
+CREATE TABLE VIDEOS(
     codigo int(6) primary key AUTO_INCREMENT,
     titulo varchar(50) not null,
     video varchar(50),
     descricao varchar (100),
-    autor varchar(50),
-    datahora varchar(20));
+    autor int(6),
+    datahora date
+);
 
-CREATE TABLE cliente(
+CREATE TABLE CLIENTE(
     codigo int(6) primary key AUTO_INCREMENT,
     nome varchar(25) not null,
     sobrenome varchar(25),
@@ -33,28 +36,23 @@ CREATE TABLE cliente(
     cpf varchar(25),
     telefone varchar (25),
     email varchar (50),
-    senha varchar (50)
+    senha varchar (50),
+    endereco int(6)
 )
 
-CREATE TABLE peso(
-    codigo int(6) primary key AUTO_INCREMENT,
-    peso varchar (25) not null,
-    produto varchar (25)
-)
-
-CREATE TABLE carrossel(
+CREATE TABLE CARROSSEL(
     codigo int(6) primary key AUTO_INCREMENT,
     imagem varchar (25) not null,
-    Autor varchar (50),
-    Datahora varchar (20)
+    autor int(6),
+    datahora date
 )
 
-CREATE TABLE administrador(
+CREATE TABLE ADMINISTRADORES(
     codigo int (6) primary key AUTO_INCREMENT,
     nome varchar (25) not null,
     email varchar (25),
     cpf varchar (25),
-    nascimento varchar (25),
-    cargo varchar (25),
+    nascimento date,
+    nivel varchar (25),
     salario varchar (25)
 )
